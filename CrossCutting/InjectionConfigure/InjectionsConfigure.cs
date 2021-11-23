1545fb568109e4012cfde34818178b12c6d70bd2
Jannielly Garcia <JanniellyGarcia21@hotmail.com>
2021-11-23 16:31:27 -0300

    Adicionar arquivos de projeto.

---
+++ CrossCutting/InjectionConfigure/InjectionsConfigure.cs
@@ -0,0 +1,23 @@
+﻿using Data.Repositories;
+using Domain.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossCutting.InjectionConfigure
+{
+    public static class Injector
+    {
+        public static void AddDependencyInjectionConfig(this IServiceCollection services)
+        {
+
+            services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+        }
+    }
+}
---
+++ Data/Context/ProjectContext.cs
@@ -0,0 +1,30 @@
+﻿using Data.Mappings;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(new UserMap().Configure);
+
+
+        }
+    }
+}
