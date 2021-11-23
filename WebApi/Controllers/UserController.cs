d9af34150695b69ebd2b9f90bd151fc4008eec5d
Jannielly Garcia <JanniellyGarcia21@hotmail.com>
2021-11-23 18:03:32 -0300

    Add controllers

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using Domain.Interface;
+using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Service.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IBaseService<User> _baseUserService;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IBaseService<User> baseUserService
+            , ILogger<UserController> logger)
+        {
+            _baseUserService = baseUserService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("CreateUser")]
+        public IActionResult Create([FromBody] User user)
+        {
+            if (user == null)
+                return NotFound();
+
+            return Execute(() => _baseUserService.Add<UserValidator>(user).Id);
+        }
+        [HttpPut]
+        public IActionResult Update([FromBody] User user)
+        {
+            if (user == null)
+                return NotFound();
+
+            return Execute(() => _baseUserService.Update<UserValidator>(user));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+                return NotFound();
+
+            Execute(() =>
+            {
+                _baseUserService.Delete(id);
+                return true;
+            });
+
+            return new NoContentResult();
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Execute(() => _baseUserService.Get());
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult Get(int id)
+        {
+            if (id == 0)
+                return NotFound();
+
+            return Execute(() => _baseUserService.GetById(id));
+        }
+
+        private IActionResult Execute(Func<object> func)
+        {
+            try
+            {
+                var result = func();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,7 +1,10 @@
+using CrossCutting.InjectionConfigure;
+using Data.
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,12 +29,13 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ProjectContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConexaoDB")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
             });
+            services.AddDependencyInjectionConfig();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
